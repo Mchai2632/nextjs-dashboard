e6bf18e82c731d823c28b1d5c1ef254cdf41fb3c
MC-MSI <s11043036@ems.tcust.edu.tw>
2025-02-23 17:29:03 +0800

    first upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Roboto, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["700"],
+});
+
+export const lusitana = Lusitana({
+    weight: ["700"],
+});
